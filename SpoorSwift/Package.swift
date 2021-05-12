c798ddf66b5dd8f884f4b5b8d49ad87af65c994f
Daniel Koster <danielkoster95@gmail.com>
2021-05-11 22:08:57 -0400

    package and apple watch support

--- SpoorSwift/Package.swift
+++ SpoorSwift/Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SpoorSwift",
-    platforms: [.iOS(.v12),
+    platforms: [.iOS(.v13),
                 .watchOS(.v5),
-                .macOS(.v10_14),
+                .macOS(.v10_15),
                 .tvOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
---
+++ SpoorSwift/SpoorSwiftWatchOS/SpoorSwiftWatchOS.h
@@ -0,0 +1,18 @@
+//
+//  SpoorSwiftWatchOS.h
+//  SpoorSwiftWatchOS
+//
+//  Created by Daniel Koster on 5/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpoorSwiftWatchOS.
+FOUNDATION_EXPORT double SpoorSwiftWatchOSVersionNumber;
+
+//! Project version string for SpoorSwiftWatchOS.
+FOUNDATION_EXPORT const unsigned char SpoorSwiftWatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpoorSwiftWatchOS/PublicHeader.h>
+
+
